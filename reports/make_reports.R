0ef51af0c7d28e1be243d53388ad5634d4c17e33
unknown <grhyne1@lsu.edu>
2022-09-09 16:13:55 -0500

    Report Version 1

--- reports/make_reports.R
+++ reports/make_reports.R
@@ -3,15 +3,16 @@ library(rmarkdown)
 library(readxl)
 
 # Define the track to consider
-gdl_list <- c("CB594")
+gdl_list <- c("CB594","CB596","CB598", "CB599","CB601","CB608","CB611","CB616", "CB619","CB620","CB621",
+              "CB623","CB624","CB627")
 # set <- read_excel("data/gdl_settings.xlsx")
 # gdl_list = set$gdl_id
 
 # Define the report to produce
-report_list <- c("basic_trajectory", "technical_details", "wind_trajectory")
+report_list <- c("basic_trajectory", "technical_details")
 
 # Generate the following report for each tracks
-# This will produce an HTML file for each reports
+# This will produce an HTML file for each report
 for (report in report_list) {
   dir.create(paste0("reports/", report), showWarnings = F)
   for (gdl in gdl_list) {
@@ -24,7 +25,7 @@ for (report in report_list) {
   }
 }
 
-# The index page (List of Report) and all the reports are then combined into a website and moved to
+# The index page (List of Reports) and all the reports are then combined into a website and moved to
 # the docs/ folder to be serve on Github easily.
 # Change _site._yml for the overall look of the website and index.Rmd for the content of the homepage
 
